583a994ddd988cd9e592f363490a55c7f13872cd
ArthurBOliveira <arthurboliveira93@gmail.com>
2017-10-22 14:53:26 -0200

    Refactoring

--- server.js
+++ server.js
@@ -69,37 +69,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-// Define routes.
-app.get('/',
-    function (req, res) {
-        res.render('home', {
-            user: req.user
-        });
-    });
-
-app.get('/login',
-    function (req, res) {
-        res.render('login');
-    });
-
-app.get('/login/facebook',
-    passport.authenticate('facebook'));
-
-app.get('/login/facebook/return',
-    passport.authenticate('facebook', {
-        failureRedirect: '/login'
-    }),
-    function (req, res) {
-        res.redirect('/');
-    });
-
-app.get('/profile',
-    require('connect-ensure-login').ensureLoggedIn(),
-    function (req, res) {
-        res.render('profile', {
-            user: req.user
-        });
-    });
+require('./server/routes.js')(app, passport);
 
 const port = process.env.PORT || 3000;
 
