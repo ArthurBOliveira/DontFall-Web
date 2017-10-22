449e19a4a529845a782eef207eaf4470764b3fd5
ArthurBOliveira <arthurboliveira93@gmail.com>
2017-10-22 14:12:35 -0200

    deploy to heroku

---
+++ config/passport.js
@@ -0,0 +1,25 @@
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
+
+// Configure the Facebook strategy for use by Passport.
+//
+// OAuth 2.0-based strategies require a `verify` function which receives the
+// credential (`accessToken`) for accessing the Facebook API on the user's
+// behalf, along with the user's profile.  The function must invoke `cb`
+// with a user object, which will be set at `req.user` in route handlers after
+// authentication.
+passport.use(new Strategy({
+    clientID: '159340584654918',
+    clientSecret: '5b858c6bbe84c21e114f8abe62480a7d',
+    callbackURL: 'http://localhost:3000/login/facebook/return'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // In this example, the user's Facebook profile is supplied as the user
+    // record.  In a production-quality application, the Facebook profile should
+    // be associated with a user record in the application's database, which
+    // allows for account linking and authentication with other identity
+    // providers.
+    return cb(null, profile);
+  }));
+
+  module.exports = {passport}
--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-const passport = require('passport');
-const Strategy = require('passport-facebook').Strategy;
 
-const publicPath = path.join(__dirname, '../public');
+let {passport} = require('./config/passport.js');
+
+const publicPath = path.join(__dirname, '../views');
 const port = process.env.PORT || 3000;
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  In a
+// production-quality application, this would typically be as simple as
+// supplying the user ID when serializing, and querying the user record by ID
+// from the database when deserializing.  However, due to the fact that this
+// example does not have a database, the complete Facebook profile is serialized
+// and deserialized.
+passport.serializeUser(function(user, cb) {
+    cb(null, user);
+  });
+  
+  passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+  });
 
 let app = express();
 app.use(passport.initialize());
@@ -13,6 +30,14 @@ app.use(passport.session()); // persistent login sessions
 let server = http.createServer(app);
 
 app.use(express.static(publicPath));
+app.set('view engine', 'ejs');
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
 require('./server/routes.js')(app, passport);
 // require('./config/passport')(passport); // pass passport for configuration
